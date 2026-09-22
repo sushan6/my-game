7449c573c2c0722bc597e54f505abc62a61e4c2e
Sushan <sushankommireddi@gmail.com>
2026-09-22 19:18:50 +0530

    Added a level

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jumpsound: AudioStreamPlayer2D = $jumpsound
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
@@ -10,14 +11,17 @@ func _physics_process(delta: float) -> void:
 	#Adds animations
 	if velocity.x > 1 or velocity.x < -1:
 		animated_sprite_2d.animation = "running"
+	else:
+			animated_sprite_2d.animation = "idle"
 	
 	# it adds the gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	
+		animated_sprite_2d.animation = "jumping"
 	# it handles the jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumpsound.play()
 	
 	
 	# gets the direcion
@@ -29,3 +33,7 @@ func _physics_process(delta: float) -> void:
 		
 	move_and_slide()
 	
+	if direction == 1.0:
+		animated_sprite_2d.flip_h = false
+	elif  direction == -1.0:
+		animated_sprite_2d.flip_h = true
